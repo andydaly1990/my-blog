edc4754c65d39f88719886a2ec4ca14bbdff3141
Andrew Daly <andaly@fexco.com>
2024-09-05 15:30:05 +0100

    set routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-     <h1>My Awesome Blog</h1>
-     <div id="page-body">
-      Welcome to by blog!
-     </div>
+        <h1>My Awesome Blog</h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />}/>
+            <Route path="/about" element={ <AboutPage />}/>
+            <Route path="/articlesPage" element={ <ArticlesListPage />}/>
+            <Route path="/articlePage" element={ <ArticlePage />}/>
+          </Routes>
+        </div>
     </div>
+    </BrowserRouter>
+     
   );
 }
 
